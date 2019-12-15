ec2778750ec5eaab133603e4360bda6e75cefdd1
brian place <bplacedrummer@gmail.com>
2019-12-14 19:08:54 -0600

    corrected cleanup script

--- packageCleanup.js
+++ packageCleanup.js
@@ -2,12 +2,10 @@ var fs = require('fs');
 var fileName = './package.json';
 var file = require(fileName);
 
-file.scripts.postinstall = '';
+delete file.scripts.postinstall;
 
 fs.writeFile(fileName, JSON.stringify(file, null, 2), function (err) {
     if (err) return console.log(err);
-    console.log(JSON.stringify(file));
-    console.log('writing to ' + fileName);
-})
+});
 
-fs.unlink('./test.js', () => {});
+fs.unlink('./packageCleanup.js', () => {});
